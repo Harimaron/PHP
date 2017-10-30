b15e264015d02b778abe2675c1511fa7be5a479a
Harimaron <32301430+Harimaron@users.noreply.github.com>
2017-10-30 11:02:13 +0100

    adding php

---
+++ php-introduction/cats.php
@@ -0,0 +1,2 @@
+<input type="button" value="Un chat" class="homebutton" id="btnHome" 
+onClick="document.location.href='index.php'" />
---
+++ php-introduction/index.php
@@ -0,0 +1,9 @@
+<input type="button" value="Plus de chat" class="homebutton" id="btnHome" 
+onClick="document.location.href='cats.php'" />
+
+<!DOCTYPE html>
+<html>
+<body>
+<img src="https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg">
+</body>
+</html>
